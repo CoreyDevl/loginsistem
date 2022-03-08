a044a8d6355b9d73861dfc06e1d94e5a0d4dd561
Raphael Alvarenga <coreydaves@hotmail.it>
2022-03-08 15:25:18 -0300

    projeto funcionando até aki

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -7,15 +7,18 @@ const api = axios.create({
 export const useApi = ()=> ({
 
   validateToken: async (token: string) => {
-
+    const response = await api.post('/validate', {token});
+    return response.data;
   },
 
   signin: async (email: string, password: string) => {
-
+    const response = await api.post('/signin', {email, password});
+    return response.data;
   },
 
   logout: async ()=> {
-    
+    const response = await api.post('/logout');
+    return response.data;
   }
 
 });
